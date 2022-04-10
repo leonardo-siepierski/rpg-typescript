e2ef3adb973e843b962c5561bcb4e78c52318e4d
leonardo <leonardo.siepierski@outlook.com>
2022-04-10 16:11:24 -0300

    req 2 missing createdracesinstancies

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,8 +2,15 @@ import Race from './Race';
 
 class Dwarf extends Race {
   lifePoints = 80;
+  counter = 0;
+
   get maxLifePoints(): number {
     return this.lifePoints;
+  }
+
+  createdRacesInstances(): number {
+    this.counter += 1;
+    return this.counter;
   }
 }
 
