bf2ef4d8f17fe7659b043d05ebd2affa675e923e
leonardo <leonardo.siepierski@outlook.com>
2022-04-11 15:38:33 -0300

    Problem on implementation of Character class. Fixing it later

--- src/Character.ts
+++ src/Character.ts
@@ -1,4 +1,4 @@
-// import Archetype from './Archetypes';
+import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
 import Race, { Elf } from './Races';
@@ -6,7 +6,7 @@ import getRandomInt from './utils';
 
 class Character implements Fighter {
   private _race: Race;
-  // private _archetype: Archetype;
+  private _archetype: Archetype;
   private _maxLifePoints: number;
   private _lifePoints: number;
   private _strength: number;
@@ -16,15 +16,74 @@ class Character implements Fighter {
 
   constructor(name: string) {
     this._race = new Elf(name, 50);
+    this._archetype = new Mage(name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
-    this._lifePoints = this._race.maxLifePoints;
+    this._lifePoints = this._maxLifePoints;
     this._dexterity = this._race.dexterity;
     this._energy = {
-      type_: 'mana',
-      amount: 50,
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
     };
     this._strength = getRandomInt(1, 10);
     this._defense = getRandomInt(1, 10);
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get energy(): Energy {
+    return this._energy;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = this._defense - attackPoints;
+
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this.validateLifePoints();
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  validateLifePoints() {
+    const sum = this._maxLifePoints + getRandomInt(1, 10);
+    if (sum > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    } else this._maxLifePoints = sum;
+  }
+
+  special(): number {
+    return this._strength * 2;
   }
 }
 
